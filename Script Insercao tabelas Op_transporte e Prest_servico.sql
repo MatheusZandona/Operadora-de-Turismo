01162049cb2c7c20cb858d204009bec361837f3c
MatheusZandona <zan_matheus@yahoo.com.br>
2016-06-23 07:08:05 -0300

    Criacao do script
    Criacao do script

---
+++ Script Insercao tabelas Op_transporte e Prest_servico.sql
@@ -0,0 +1,44 @@
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Companhia x da Maria", "Cascavel, Corbélia, ubiratã", 23, 10, "Objetos Pontiagudos, armas de fogo", 20.00, 0 , "A");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Companhia H do João", "Cascavel, Foz do Iguaçu, Curitiba", 26, 5, "Objetos Pontiagudos, armas de fogo", 50.00, 0 , "A");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Companhia Y da Joana", "Curitiba, Rio de janeiro, Ceara", 60, 20, "Objetos Pontiagudos, armas de fogo, chicletes", 10.00, 0 , "A");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Companhia c da Lucao", "Cascavel, Sao Paulo, fortaleza", 90, 2, "Objetos Pontiagudos, armas de fogo", 1000.00, 0 , "A");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Princesa dos Campos", "Cascavel, Corbélia, ubiratã", 0, 0, "", 20.00, 5 , "T");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Eucatur", "Cascavel, Sao paulo, Rio de janeiro", 22, 10, "", 0, 5 , "T");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Princesa dos Campos", "Cascavel, Sei lah, Rio de janeiro", 15, 10, "", 0, 5 , "T");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Kaiwoa", "Cascavel, Mato Grosso, Acre", 22, 10, "", 0, 5 , "T");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Aguia Branca", "Cascavel, Brasilia, Porto Velho", 22, 2, "", 0, 5 , "T");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Azul", "Cascavel, Foz, Curitiba, BH", 120, 39, "Objetos Pontiagudos, armas de fogo", 20.00, 0 , "A");
+insert into op_transporte (nome, trajeto, num_acentos, num_acentos_disp, inf_restricoes, taxa_add_embarque, num_acentos_min, tipo) 
+values ("Tam", "Cascavel, Foz, Sao Paulo, Brasilia", 150, 50, "Objetos Pontiagudos, armas de fogo", 200.00, 0 , "A");
+
+
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("Abc", "(45) 9966-6655", "(45)3098-8865", "08.252.076/0001-50", "abc@gmai.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("Def", "(45) 9912-9932", "(44)3328-8985", "09.888.021/0001-90", "def@gmai.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("Ghi", "(99) 9999-0000", "(00)5555-3333", "00.444.999/0001-32", "ghi@hotmail.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("jkl", "(45) 5555-9999", "(67)1234-9876", "46.876.125/0002-00", "jkl01@outlook.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("mno", "(45) 2233-4455", "(41)4321-6543", "09.654.098/0001-22", "mno10@yahoo.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("pqr", "(45) 5555-99766", "(43)09876-2345", "32.654.123/0001-91", "pqr@gmai.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("Def", "(45) 9912-9932", "(44)3328-8985", "09.888.021/0001-90", "def@gmai.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("Ghi", "(99) 9999-0000", "(00)5555-3333", "00.444.999/0001-32", "ghi@hotmail.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("jkl", "(45) 5555-9999", "(67)1234-9876", "46.876.125/0002-00", "jkl01@outlook.com");
+insert into prest_servico (nome, tel_celular, telefone, cnpj, email) 
+values ("mno", "(45) 2233-4455", "(41)4321-6543", "09.654.098/0001-22", "mno10@yahoo.com");
