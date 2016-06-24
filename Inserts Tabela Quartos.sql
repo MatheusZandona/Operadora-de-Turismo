83a94f30f9c49b932b82e8a03952e967d6a7c4e1
Evandro Pimentel de Areis <evandro-96@live.com>
2016-06-24 11:34:52 -0300

    Insert tabela quartos
    tabela quartos

---
+++ Inserts Tabela Quartos.sql
@@ -0,0 +1,15 @@
+USE opturismo;
+
+INSERT INTO quartos VALUES(1, "Familia", 1, 2, 101, 1, 100.00, "quarto para familia de até 4 pessoas");
+INSERT INTO quartos VALUES(2, "Casal", 1, 0, 102, 1, 100.00, "quarto para casal");
+INSERT INTO quartos VALUES(3, "Casal Premium", 1, 0, 103, 1, 150.00, "quarto para casal");
+INSERT INTO quartos VALUES(4, "Casal Deluxe", 1, 0, 104, 1, 200.00, "quarto para casal");
+INSERT INTO quartos VALUES(5, "Familia Premium", 1, 2, 201, 2, 250.00, "quarto para familia de até 4 pessoas");
+INSERT INTO quartos VALUES(6, "Solteiro", 0, 2, 202, 2, 89.00, "quarto para 2 pessoas");
+INSERT INTO quartos VALUES(7, "Solteiro Premium", 0, 2, 203, 2, 120.00, "quarto para 2 pessoas");
+INSERT INTO quartos VALUES(8, "Solteiro Deluxe", 0, 2, 204, 2, 160.00, "quarto para 2 pessoas");
+INSERT INTO quartos VALUES(9, "Familia Economic", 1, 2, 301, 3, 80.00, "quarto para familia de até 4 pessoas");
+INSERT INTO quartos VALUES(10, "Casal Economic", 1, 0, 302, 3, 89.00, "quarto para casal");
+
+
+
