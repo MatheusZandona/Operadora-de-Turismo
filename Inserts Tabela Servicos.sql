14c90b06e411eb6e45d6fafa196a422e1b7cac94
LucasMedeiros10 <lucas_medeiros09@hotmail.com>
2016-06-24 00:04:58 -0300

    Script tabela Servicos

---
+++ Inserts Tabela Servicos.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 4.1.22-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('1','Translado','Translado de passageiros','20');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('2','Alimentação','Alimentação por pessoa','27');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('3','Guia','Guia turístico','7');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('4','Fotos','Álbum de fotos da viagem','550');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('5','Carro','Aluguel de veículo(diário)','120');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('6','Quarto adicional','Diária de quarto adicional','150');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('7','Corcovado','Ingresso para passeio ao corcovado','80');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('8','Ultra Leve','Passeio de ultra leve','350');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('9','Cataratas','Passeio nas Cataratas do Iguaçu','30,00');
+insert into `servicos` (`ID`, `NOME`, `DESCRICAO`, `VALOR`) values('10','Parque Aquático','Parque Aquático','103,00');
