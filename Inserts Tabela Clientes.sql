7c4580abdc3b28fa8a2fd1b3144bf09dc6f2846d
LucasMedeiros10 <lucas_medeiros09@hotmail.com>
2016-06-24 00:29:18 -0300

    Script Inserts Clientes

---
+++ Inserts Tabela Clientes.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 4.1.22-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('1','João','Cabral','10.294.546-9','087.654.345-78','1976-06-09','joaocabral@hotmail.com','(45)9987-6588','(45)6554-2653','(45)2365-2653','Solteiro');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('2','Maria','Lima','23.263.265-7','736.343.938-98','1993-03-03','mariazinha@gmail.com','(45)9987-9887','(45)8127-2652','(45)2365-7232','Casado');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('3','Caroline','Alves','23.232.343-8','113.283.276-23','1990-05-20','carol.alves@yahoo.com','(65)2635-72637','(65)8897-2161','(65)2324-2323','Viuvo');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('4','Marcos','Limeira','12.928.726-9','132.127.253-23','1975-12-12','mlimeira@bol.com','(23)9987-2763','(68)3683-4852','(23)2322-2323','Solteiro');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('5','Emanoel','Assunção','38.736.635-9','763.736.376-34','1987-08-17','assuncao@assuncaobebidas.com.br','(87)9834-7364','(81)3745-2753','(87)4587-4875','Solteiro');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('6','Guilherme','Silva','98.232.234.5','763.723.213-87','1999-11-12','guisilva@msn.com','(67)9893-3746','(92)3997-1249','(67)3453-3463','Casado');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('7','Bruno','Vidal','13.237.237-9','712.362.726-28','1991-01-01','bruno_vidal@hotmail.com','(76)2635-2636','(28)3891-2989','(76)3476-7364','Divorciado');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('8','Paulo','da Rosa','87.273.726-7','795.102.740-54','1966-03-03','darosa@hotmail.com','(65)6534-3746','(65)3525-1226','(65)3486-3846','Casado');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('9','Larissa','Martins','23.237.276-2','527.948.040-14','1980-05-24','larimartins@gmail.com','(69)8347-3476','(69)3537-6771','(69)6354-7364','Divorciado');
+insert into `cliente` (`ID`, `NOME`, `SOBRENOME`, `RG`, `CPF`, `DT_NASCIMENTO`, `EMAIL`, `TEL_CELULAR`, `TEL_RESIDENCIAL`, `TEL_TRABALHO`, `ESTADO_CIVIL`) values('10','Jaqueline','Marin','12.235.276-3','010.939.178-06','1987-10-04','jaque_martins@gmail.com','(76)3455-3676','(41)3758-4475','(76)3764-7364','Viuvo');
