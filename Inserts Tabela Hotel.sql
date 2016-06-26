41737bbaa5bb39564c8dbfa41d43add51500e6a8
MatheusZandona <zan_matheus@yahoo.com.br>
2016-06-26 15:59:30 -0300

    Inerts Tabela Hotel feito por Mychel Rennó
    Inserts

---
+++ Inserts Tabela Hotel.sql
@@ -0,0 +1,31 @@
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Copas Executive', 'av brasil', 12345, 'centro', 'cascavel', 'pr', 'brasil', 200, 4, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Bourbon', 'av brasil', 54321, 'centro', 'cascavel', 'pr', 'brasil', 150, 4, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Caiua', 'rua parana', 67890, 'centro', 'cascavel', 'pr', 'brasil', 160, 4, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Copas verdes', 'rua parana', 76549, 'centro', 'cascavel', 'pr', 'brasil', 410, 4, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Plaza', 'rua parana', 19843, 'centro', 'cascavel', 'pr', 'brasil', 110, 4, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Deville', 'rua rio grande do sul', 65438, 'centro', 'cascavel', 'pr', 'brasil', 200, 3, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Regente', 'rua rio grande do sul', 76754, 'centro', 'cascavel', 'pr', 'brasil', 180, 3, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Querencia', 'rua sao paulo', 87657, 'centro', 'cascavel', 'pr', 'brasil', 120, 3, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Grand Prix', 'av brasil', 13242, 'centro', 'cascavel', 'pr', 'brasil', 90, 3, 't');
+
+INSERT INTO hotel(nome, end_rua, end_numero, end_bairro, end_cidade, end_uf, end_pais, num_quartos, classific_embra, alimentacao) 
+VALUES ('Maestro', 'rua sao paulo', 98978, 'centro', 'cascavel', 'pr', 'brasil', 85, 3, 't');
+
+select * from hotel;
