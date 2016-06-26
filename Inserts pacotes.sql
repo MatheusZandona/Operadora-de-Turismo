a022382406cac5817c2e6422f60418185e58e439
michaelcoitim <michaelcoitim1@gmail.com>
2016-06-26 09:26:59 -0300

    Add files via upload

---
+++ Inserts pacotes.sql
@@ -0,0 +1,10 @@
+INSERT INTO pacotes VALUES(1, 'basico', 1);
+INSERT INTO pacotes VALUES(2, 'basico moderado', 2);
+INSERT INTO pacotes VALUES(3, 'basico premium', 3);
+INSERT INTO pacotes VALUES(4, 'basico zafira', 4);
+INSERT INTO pacotes VALUES(5, 'premium', 5);
+INSERT INTO pacotes VALUES(6, 'premium moderado', 6);
+INSERT INTO pacotes VALUES(7, 'premium zafira', 7);
+INSERT INTO pacotes VALUES(8, 'master ', 8);
+INSERT INTO pacotes VALUES(9, 'mater premium ', 9);
+INSERT INTO pacotes VALUES(10,'master zafira ', 10
